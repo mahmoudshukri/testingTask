a482ffe83cdd6bc52854cd9d340af8fa9872b033
mahmoudshukri <mahmoudshukri.ms@gmail.com>
2022-02-01 22:42:13 +0200

    testingTask

---
+++ Omni-Notes-develop/omniNotes/src/androidTest/java/it/feio/android/omninotes/testingTask.java
@@ -0,0 +1,291 @@
+package it.feio.android.omninotes;
+
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.longClick;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class testingTask {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testingTask() {
+        ViewInteraction viewInteraction = onView(
+                allOf(withId(R.id.fab_expand_menu_button),
+                        childAtPosition(
+                                allOf(withId(R.id.fab),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.FrameLayout")),
+                                                2)),
+                                3),
+                        isDisplayed()));
+        viewInteraction.perform(click());
+
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.fab_note),
+                        childAtPosition(
+                                allOf(withId(R.id.fab),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.FrameLayout")),
+                                                2)),
+                                2),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+
+        ViewInteraction editText = onView(
+                allOf(withId(R.id.detail_title),
+                        childAtPosition(
+                                allOf(withId(R.id.title_wrapper),
+                                        childAtPosition(
+                                                withId(R.id.detail_tile_card),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        editText.perform(replaceText("Test"), closeSoftKeyboard());
+
+        ViewInteraction editText2 = onView(
+                allOf(withId(R.id.detail_content),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.fragment_detail_content),
+                                        1),
+                                0)));
+        editText2.perform(scrollTo(), replaceText("Content1"), closeSoftKeyboard());
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("drawer open"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.RelativeLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction viewInteraction2 = onView(
+                allOf(withId(R.id.fab_expand_menu_button),
+                        childAtPosition(
+                                allOf(withId(R.id.fab),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.FrameLayout")),
+                                                2)),
+                                3),
+                        isDisplayed()));
+        viewInteraction2.perform(click());
+
+        ViewInteraction floatingActionButton2 = onView(
+                allOf(withId(R.id.fab_checklist),
+                        childAtPosition(
+                                allOf(withId(R.id.fab),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.FrameLayout")),
+                                                2)),
+                                1),
+                        isDisplayed()));
+        floatingActionButton2.perform(click());
+
+        ViewInteraction editText3 = onView(
+                allOf(withId(R.id.detail_title),
+                        childAtPosition(
+                                allOf(withId(R.id.title_wrapper),
+                                        childAtPosition(
+                                                withId(R.id.detail_tile_card),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        editText3.perform(replaceText("Test2"), closeSoftKeyboard());
+
+        ViewInteraction editTextMultiLineNoEnter = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withClassName(is("it.feio.android.checklistview.models.CheckListViewItem")),
+                                0),
+                        2),
+                        isDisplayed()));
+        editTextMultiLineNoEnter.perform(replaceText("Item1"), closeSoftKeyboard());
+
+        ViewInteraction editTextMultiLineNoEnter2 = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withClassName(is("it.feio.android.checklistview.models.CheckListViewItem")),
+                                0),
+                        2),
+                        isDisplayed()));
+        editTextMultiLineNoEnter2.perform(replaceText("Item2"), closeSoftKeyboard());
+
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(withContentDescription("drawer open"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.RelativeLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton2.perform(click());
+
+        ViewInteraction frameLayout = onView(
+                allOf(withId(R.id.root),
+                        childAtPosition(
+                                allOf(withId(R.id.list),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.FrameLayout")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        frameLayout.perform(longClick());
+
+        ViewInteraction overflowMenuButton = onView(
+                allOf(withContentDescription("More options"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.action_mode_bar),
+                                        2),
+                                3),
+                        isDisplayed()));
+        overflowMenuButton.perform(click());
+
+        ViewInteraction materialTextView = onView(
+                allOf(withId(R.id.title), withText("Trash"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialTextView.perform(click());
+
+        ViewInteraction appCompatImageButton3 = onView(
+                allOf(withContentDescription("drawer open"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.RelativeLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton3.perform(click());
+
+        DataInteraction linearLayout = onData(anything())
+                .inAdapterView(allOf(withId(R.id.drawer_nav_list),
+                        childAtPosition(
+                                withId(R.id.left_drawer),
+                                1)))
+                .atPosition(1);
+        linearLayout.perform(scrollTo(), click());
+
+        ViewInteraction appCompatImageButton4 = onView(
+                allOf(withContentDescription("drawer open"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.RelativeLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton4.perform(click());
+
+        ViewInteraction linearLayout2 = onView(
+                allOf(withId(R.id.settings_view),
+                        childAtPosition(
+                                allOf(withId(R.id.left_drawer),
+                                        childAtPosition(
+                                                withId(R.id.navigation_drawer),
+                                                0)),
+                                2)));
+        linearLayout2.perform(scrollTo(), click());
+
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.recycler_view),
+                        childAtPosition(
+                                withId(android.R.id.list_container),
+                                0)));
+        recyclerView.perform(actionOnItemAtPosition(4, click()));
+
+        ViewInteraction recyclerView2 = onView(
+                allOf(withId(R.id.recycler_view),
+                        childAtPosition(
+                                withId(android.R.id.list_container),
+                                0)));
+        recyclerView2.perform(actionOnItemAtPosition(3, click()));
+
+        ViewInteraction appCompatImageButton5 = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.RelativeLayout")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton5.perform(click());
+
+        ViewInteraction appCompatImageButton6 = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.RelativeLayout")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton6.perform(click());
+
+        pressBack();
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
